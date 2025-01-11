20ac473b156e3435d15ddbadb254267c14f66936
yawarasuuna <yawarasuuna@proton.me>
2025-01-10 22:39:44 -0300

    adds validation to MinimumAccount

--- src/ethereum/MinimumAccount.sol
+++ src/ethereum/MinimumAccount.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.25;
 
 import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
@@ -9,14 +10,42 @@ import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/Messa
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MinimalAccount is IAccount, Ownable {
-    constructor() Ownable(msg.sender) {}
+    /*//////////////////////////////////////////////////////////////
+                                 Errors
+    //////////////////////////////////////////////////////////////*/
+
+    error MinimalAccount__NotFromEntryPoint();
+
+    // address private immutable i_entryPoint;
+    IEntryPoint private immutable i_entryPoint;
+
+    modifier requireFromEntryPoiunt() {
+        if (msg.sender != address(i_entryPoint)) {
+            revert MinimalAccount__NotFromEntryPoint();
+        }
+        _;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                               Functions
+    //////////////////////////////////////////////////////////////*/
+
+    constructor(address entryPoint) Ownable(msg.sender) {
+        // i_entryPoint = entryPoint
+        i_entryPoint = IEntryPoint(entryPoint);
+    }
 
     // Signature is valid if it is the MinimalAccount contract owner,  can go wild
     function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
         external
+        requireFromEntryPoiunt
         returns (uint256 validationData)
     {
-        _validateSignature(userOp, userOpHash);
+        validationData = _validateSignature(userOp, userOpHash);
+        // _validateNonce(); PackedUserOperation should have a nonce, and we can keep track of it and validate it
+        // eg it needs to be sequential, in order, any order, it needs to be in a mapping, do whatever we want
+        // nonce uniquiness is managed by the entryPoint itself
+        _payPrefund(missingAccountFunds); // or use payMaster
     }
 
     // EIP-191 version of signed hash
@@ -33,5 +62,21 @@ contract MinimalAccount is IAccount, Ownable {
             return SIG_VALIDATION_FAILED;
         }
         return SIG_VALIDATION_SUCCESS;
+    }
+
+    function _payPrefund(uint256 missingAccountFunds) internal {
+        if (missingAccountFunds != 0) {
+            // hardcode msg.sender as entryPoint contract, its the entryPoint contract's job to verify payment is good
+            (bool sucess,) = payable(msg.sender).call{value: missingAccountFunds, gas: type(uint256).max}("");
+            (sucess);
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                Getters
+    //////////////////////////////////////////////////////////////*/
+
+    function getEntryPoint() internal view returns (address) {
+        return address(i_entryPoint);
     }
 }
